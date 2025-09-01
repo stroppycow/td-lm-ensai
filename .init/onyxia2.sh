f667df8e80d97d4a7fd5632ddd41c27f87cdfd3c
Théo Leroy <theo.leroy@insee.de>
2025-09-01 22:39:48 +0200

    Init RStudio

--- .init/onyxia2.sh
+++ .init/onyxia2.sh
@@ -6,7 +6,7 @@ setHook('rstudio.sessionInit', function(newSession) {
   if (newSession && identical(getwd(), '${WORKSPACE_DIR}'))
   {
     message('Activation du projet RStudio')
-    rstudioapi::openProject('${WORKSPACE_DIR}/td-lm-ensai')
+    rstudioapi::openProject('${WORKSPACE_DIR}/td-lm-ensai', newSession = TRUE)
   }
   if(identical(getwd(), '${WORKSPACE_DIR}/td-lm-ensai')){
     rstudioapi::sendToConsole('options(repos = c(RSPM = \"https://packagemanager.posit.co/cran/latest\"))', execute = TRUE)
