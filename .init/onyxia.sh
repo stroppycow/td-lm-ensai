5d3063d3e7b13f9a21c1b117e1e877f5e33befef
Théo Leroy <theo.leroy@insee.de>
2025-09-01 21:53:03 +0200

    Init RStudio

--- .init/onyxia.sh
+++ .init/onyxia.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
-  if (newSession && identical(getwd(), 'td-lm-ensai'))
+  if (newSession && identical(getwd(), '/home/onyxia'))
   {
     message('Activation du projet RStudio')
-    rstudioapi::openProject('td-lm-ensai')
+    rstudioapi::openProject('/home/onyxia/td-lm-ensai')
   }
 }, action = 'append')
 " >> /home/onyxia/.Rprofile
