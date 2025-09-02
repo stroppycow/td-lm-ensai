fe931b7621658def68577c6f7c7cfd10da70f9e8
Théo Leroy <theo.leroy@insee.de>
2025-09-02 23:39:26 +0200

    TD 1

--- .init/onyxia4.sh
+++ .init/onyxia4.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-cd td-lm-ensai
-R -e "options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')" -e  "renv::restore()"
-tlmgr update --self && tlmgr install luatex85 && tlmgr install tkz-tab
-cd ..
+# cd td-lm-ensai
+# R -e "options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')" -e  "renv::restore()"
+# tlmgr update --self && tlmgr install luatex85 && tlmgr install tkz-tab
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
-  print(getwd())
   if (newSession && identical(getwd(), '${WORKSPACE_DIR}'))
   {
     message('Activation du projet RStudio')
     rstudioapi::openProject('${WORKSPACE_DIR}/td-lm-ensai', newSession = FALSE)
   }
 }, action = 'append')
-
 " >> /home/onyxia/.Rprofile
