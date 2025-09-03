ad656ff0848befb0d6f29868f964ede7f27a8f1a
stroppycow <theo.leroy@insee.fr>
2025-09-03 05:53:38 +0000

    Test renv automatisé

--- .init/onyxia5.sh
+++ .init/onyxia5.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get install -y inkscape
 tlmgr update --self && tlmgr install luatex85 && tlmgr install tkz-tab
-cd td-lm-ensai
-R -e "options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')" -e  "renv::restore(prompt = FALSE)"
-cd ..
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
   if (newSession && identical(getwd(), '${WORKSPACE_DIR}'))
   {
     message('Activation du projet RStudio')
-    rstudioapi::openProject('${WORKSPACE_DIR}/td-lm-ensai', newSession = FALSE)
+    rstudioapi::openProject('${WORKSPACE_DIR}/td-lm-ensai', newSession = TRUE)
   }
 }, action = 'append')
 " >> /home/onyxia/.Rprofile
+echo \ 
+"
+options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
+renv::restore(prompt = FALSE)
+" >> /home/onyxia/td-lm-ensai/.Rprofile
