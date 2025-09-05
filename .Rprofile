95c003b469ada756da2c414902ab057e6110d50b
stroppycow <theo.leroy@insee.fr>
2025-09-05 22:33:25 +0000

    TD 1

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,5 @@
 source("renv/activate.R")
+
+options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
+renv::restore(prompt = FALSE)
+
--- .init/onyxia7.sh
+++ .init/onyxia7.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get install -y inkscape
 tlmgr update --self && tlmgr install luatex85 && tlmgr install tkz-tab
+curl -sSL https://git.io/get-mo -o mo
+chmod +x mo
+sudo mv mo /usr/local/bin/
+QUARTO_PREVIEW_URL=$1
+export QUARTO_PREVIEW_URL=QUARTO_PREVIEW_URL
+mo ./td-lm-ensai/.kube/ingress_template.yaml > ./td-lm-ensai/.kube/ingress_output.yaml
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
@@ -15,4 +21,14 @@ echo \
 "
 options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
 renv::restore(prompt = FALSE)
+
+rstudioapi::terminalExecute(
+   command = 'kubectl apply -f ./.kube/ingress_output.yaml'
+)
+
+rstudioapi::terminalExecute(
+   command = 'quarto preview --host 0.0.0.0 --port 5000'
+)
 " >> /home/onyxia/work/td-lm-ensai/.Rprofile
+
+
