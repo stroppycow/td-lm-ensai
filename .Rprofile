1b0dc3b2aa03a776f1bcfdd8e2629850fd6c412e
stroppycow <theo.leroy@insee.fr>
2025-09-05 22:57:21 +0000

    TD 1

--- .Rprofile
+++ .Rprofile
@@ -1,5 +1,15 @@
 source("renv/activate.R")
 
-options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
-renv::restore(prompt = FALSE)
+if(Sys.getenv("ONYXIA_MODE", unset = "") == "1"){
+  options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
+  renv::restore(prompt = FALSE)
+  
+  rstudioapi::terminalExecute(
+    command = 'kubectl apply -f ./../ingress_output.yaml'
+  )
+  
+  rstudioapi::terminalExecute(
+    command = 'quarto preview --host 0.0.0.0 --port 5000'
+  )
+}
 
--- .init/onyxia7.sh
+++ .init/onyxia7.sh
@@ -5,8 +5,9 @@ curl -sSL https://git.io/get-mo -o mo
 chmod +x mo
 sudo mv mo /usr/local/bin/
 QUARTO_PREVIEW_URL=$1
-export QUARTO_PREVIEW_URL=QUARTO_PREVIEW_URL
-mo ./td-lm-ensai/.kube/ingress_template.yaml > ./td-lm-ensai/.kube/ingress_output.yaml
+export QUARTO_PREVIEW_URL=${QUARTO_PREVIEW_URL}
+export ONYXIA_MODE=1
+mo ./td-lm-ensai/.kube/ingress_template.yaml > ./ingress_output.yaml
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
@@ -17,18 +18,3 @@ setHook('rstudio.sessionInit', function(newSession) {
   }
 }, action = 'append')
 " >> /home/onyxia/.Rprofile
-echo \
-"
-options(renv.config.repos.override = 'https://packagemanager.posit.co/cran/latest')
-renv::restore(prompt = FALSE)
-
-rstudioapi::terminalExecute(
-   command = 'kubectl apply -f ./.kube/ingress_output.yaml'
-)
-
-rstudioapi::terminalExecute(
-   command = 'quarto preview --host 0.0.0.0 --port 5000'
-)
-" >> /home/onyxia/work/td-lm-ensai/.Rprofile
-
-
